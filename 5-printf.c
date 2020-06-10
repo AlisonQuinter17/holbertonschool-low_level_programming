95fbe6111854dfd5bf1c38af22992c46918dbcae
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-06-10 10:09:06 -0700

    This C program prints a sentence using printf, followed by a new line.

---
+++ 5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints a sentence followed by a new line, using printf.
+ *
+ * Return: Always (0).
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+		return (0);
+}
