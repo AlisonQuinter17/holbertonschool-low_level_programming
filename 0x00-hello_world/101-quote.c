76234a5e4aafbbdf7e5690facf4a2e9d383f4903
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-06-10 13:42:05 -0700

    This program prints exactly a sentence by Dora Korpar, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - This program prints exactly the sentence of Dora Korpar,
+ * followed by a new line, to the standard error.
+ *
+ * Return: (1) to error.
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
