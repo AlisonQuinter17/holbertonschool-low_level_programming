00cbe5280e759132d16218c9e0679a54e5cdb87d
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-06-11 20:43:30 -0700

    This program prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - This program prints the numbers from 00 to 99.
+ * main - This program prints all possible different combinations of two digits.
  *
  * Return: 0.
  */
