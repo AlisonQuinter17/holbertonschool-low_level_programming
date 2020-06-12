f188dc192affc605e8200f42bc3373ca84067b61
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-06-11 18:24:27 -0700

    This program prints the numbers from 0 to 9.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program print the numbers from 0 to 9.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+		printf("%i", i);
+
+	return (0);
+}
