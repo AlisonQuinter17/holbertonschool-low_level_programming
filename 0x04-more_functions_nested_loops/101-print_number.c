cd4be37ba0fbb01dba1c3d093372b23695cfc26f
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-06-18 13:40:21 -0700

    (Advanced) This program prints any integer using _putchar.

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * print_number - main function.
+ * @n: number.
+ *
+ * Description: This function prints an integer.
+ * Return: none.
+ */
+
+void print_number(int n)
+{
+	int i;
+	int r;
+	int p;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = n * -1;
+	}
+
+	else
+	{
+		i = n;
+	}
+
+	p = 1;
+	r = i;
+
+	while (r > 9)
+	{
+		p *= 10;
+		r /= 10;
+	}
+
+	for (; p >= 1; p /= 10)
+	{
+		_putchar(((i / p) % 10) + '0');
+	}
+}
