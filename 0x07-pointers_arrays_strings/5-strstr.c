b1fdd13e37d153db2863e1ddfbf8138be28bfab1
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-06-30 18:42:11 -0700

    This program prints all possible different combinations of three digits.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,9 +15,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
 
-	if (*needle == '\0')
+	if (needle == 0 || *needle == '\0')
 	{
-		return(0);
+		return (0);
 	}
 
 	while (needle[i])
