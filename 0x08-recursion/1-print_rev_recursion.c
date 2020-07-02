f2fbe7dcdd25a67102f86905c90d178471f2dcd2
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-01 18:20:55 -0700

    This function prints a string in reverse secursively.

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - main function.
+ * @s: Pointer to the string.
+ *
+ * Description: This function prints a string in reverse.
+ *
+ * Return: None.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+
+	else
+	{
+		_putchar ('\0');
+	}
+}
