d597a261ce6c02704dc273e6408dca65829bf11f
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-05 14:32:51 -0700

    This program prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - main function.
+ * @argc: The argument count.
+ * @argv: The argument vector.
+ *
+ * Description: This program that prints its name, followed by a new line.
+ *
+ * Return: Zero.
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
