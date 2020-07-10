10c5e30239c409e26fbe326ad04778044b67dd96
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-09 20:23:39 -0700

    This program prints

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,77 @@
+#include "holberton.h"
+
+/**
+ * *string_nconcat - main function.
+ * @s1: Pointer to the first string (Betty ).
+ * @s2: Pointer to the second string (Holberton).
+ * @n: Number.
+ *
+ * Description: This function concatenates two strings.
+ * Return: The pointer to the concatenated string.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int h = _strlen(s1), s = _strlen(s2);
+	int i = 0, size;
+	unsigned int j = 0;
+	char *p;
+
+	if (!s1)
+		s1 = "";
+
+	if (!s2)
+		s2 = "";
+
+	size = h + s + 1;
+
+	p = malloc(sizeof(char) * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < size - 1)
+	{
+		if (s1[i])
+		{
+			p[i] = s1[i];
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+
+	while (j < n)
+	{
+		if (s2[j])
+			p[i] = s2[j];
+
+		i++, j++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+
+/**
+ * _strlen - main function.
+ * @s: Pointer that runs through the string.
+ *
+ * Description: This function counts the length of a string.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
