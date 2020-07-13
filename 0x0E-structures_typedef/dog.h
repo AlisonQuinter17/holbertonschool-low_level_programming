cecfd2579ed0e5e9150fec41f48330e6b8c27ef9
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-13 10:16:57 -0700

    Header file.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,19 @@ struct dog
 };
 
 
+
+/* Prototypes */
+
+typedef struct dog dog_t;
+
+int _putchar(char c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
