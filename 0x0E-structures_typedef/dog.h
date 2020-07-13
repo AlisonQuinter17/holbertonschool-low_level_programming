3e09696cd499f316302d3ee72b9969a3b4f1f31f
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-13 10:12:52 -0700

    Header file.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef DJANGO_H
+#define DJANGO_H
+
+/**
+ * struct dog - The structure of the dog.
+ * @name: The dog's name.
+ * @age: The dog's age.
+ * @owner: The dog owner's name.
+ *
+ * Description: It is the structure of an adorable puppy named Django.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+/*
+int _putchar(char c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);*/
+
+#endif
