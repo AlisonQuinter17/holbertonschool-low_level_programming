1155a72e499d44ffb9050d9a344c94c03cb9f7d6
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-13 10:13:47 -0700

    Header file.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,7 +9,6 @@
  *
  * Description: It is the structure of an adorable puppy named Django.
  */
-
 struct dog
 {
 	char *name;
@@ -17,16 +16,5 @@ struct dog
 	char *owner;
 };
 
-
-/*
-int _putchar(char c);
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);*/
 
 #endif
