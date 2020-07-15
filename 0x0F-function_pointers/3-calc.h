ee4a7328a07cf48f06f6051337c9e14c3cdbcd82
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-15 09:23:30 -0700

    Header file of simple operations.

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,16 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+#endif
