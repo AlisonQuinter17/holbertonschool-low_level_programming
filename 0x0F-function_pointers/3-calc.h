05fd5c5ad88c8329df8d5ec3e2834db6616713c4
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-15 21:50:03 -0700

    Header file of simple operations.

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,12 @@
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
+/* libraries */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
 /**
  * struct op - Struct op
  *
@@ -9,8 +15,23 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
+
+
+/* Prototypes */
+
+int (*get_op_func(char *s))(int, int);
+
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
 
 #endif
