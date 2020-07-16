eba8498338e98d62f6fa86572fd3d2cf213a9672
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-15 21:43:49 -0700

    Test file.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,16 @@
+#include "3-calc.h"
+
+/**
+ * main - main function
+ * @argc: The argument count.
+ * @argv: The argument vector.
+ *
+ * Description: This program perform simple operations.
+ *
+ * Return: 0.
+ */
+
+int main(int *argc, char *argv[])
+{
+
+}
