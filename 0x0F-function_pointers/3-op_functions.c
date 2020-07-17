49ff2dd89d789dcbffb63d2c2eb2331cd7ca972f
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-16 19:43:30 -0700

    This file contain the 5 functions to operate.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,80 @@
+#include "3-calc.h"
+
+/**
+ * op_add - main function
+ * @a: The first parameter.
+ * @b: The second parameter.
+ *
+ * Description :This function adds the two parameters.
+ *
+ * Return: The sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - main function
+ * @a: The first parameter.
+ * @b: The second parameter.
+ *
+ * Description :This function subtracts the two parameters.
+ *
+ * Return: The difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - main function
+ * @a: The first parameter.
+ * @b: The second parameter.
+ *
+ * Description :This function multiplies the two parameters.
+ *
+ * Return: The product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - main function
+ * @a: The first parameter.
+ * @b: The second parameter.
+ *
+ * Description :This function divide the two parameters.
+ *
+ * Return: The result of the division of a and b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - main function
+ * @a: The first parameter.
+ * @b: The second parameter.
+ *
+ * Description :This function modulates the two parameters.
+ *
+ * Return: The remainder of the division of a and b.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
