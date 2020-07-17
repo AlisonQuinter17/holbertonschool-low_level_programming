164c2c970780d84a2d924d4b2dd723a27b97a6e8
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-16 21:57:39 -0700

    Test file.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,39 @@
  * Return: 0.
  */
 
-int main(int *argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	int num1, num2, process;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (argv[2][1] != 0 ||
+	    (argv[2][0] != '+' &&
+	    argv[2][0] != '-' &&
+	    argv[2][0] != '*' &&
+	    argv[2][0] != '/' &&
+	    argv[2][0] != '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	process = (*get_op_func(argv[2]))(num1, num2);
+	{
+		printf("%d\n", process);
+	}
+	return (0);
 }
