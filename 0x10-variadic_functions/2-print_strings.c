bff1ee25ae47240adbcf0c1563fc3a197391cf96
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-17 11:29:18 -0700

    This function prints strings with separator.

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - main function
+ * @separator: Pointer to the string separator.
+ * @n: The number of arguments.
+ * @...: The strings to print.
+ *
+ * Description: This function prints strings, followed by a new line.
+ *
+ * Return: Nothing.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+}
