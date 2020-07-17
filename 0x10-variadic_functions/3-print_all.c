9918eb19b80c75d94caf1b2252a31eb1deadd62b
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-17 11:34:31 -0700

    This function prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - main function
+ * @format: ...
+ *
+ * Description: This function prints anything.
+ *
+ * Return: Nothing.
+ */
+
+void print_all(const char * const format, ...)
+{
+
+}
