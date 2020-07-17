aa92b678a6fb73a3b37e8915e6edf26d4b1340f2
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-17 11:13:28 -0700

    This function prints numbers with separator.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - main function
+ * @separator: Pointer to the string separator.
+ * @n: The number of arguments.
+ * @...: The integrers to print.
+ *
+ * Description: This function prints numbers, followed by a new line.
+ *
+ * Return: Nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
