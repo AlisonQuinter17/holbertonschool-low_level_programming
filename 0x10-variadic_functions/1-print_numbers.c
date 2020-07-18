8ace2c18fb644ae5aa09b93dbda54090c94d70b8
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-17 17:51:40 -0700

    This function prints numbers with separator.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,5 +13,21 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list p;
+	int i = 0;
 
+	va_start(p, n);
+
+	while (i < (int)n)
+	{
+		printf("%d", va_arg(p, int));
+
+		if (separator && i != ((int)n - 1))
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(p);
 }
