cef287c62882f1fbbe3f86be1a81fa614c1f2e3b
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-18 10:53:26 -0700

    This function prints strings with separator.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -36,7 +36,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", va_arg(st, char*));
 		}
 
-		if (separator && i != ((int)n - 1))
+		if (separator != NULL && i != ((int)n - 1))
 		{
 			printf("%s", separator);
 		}
