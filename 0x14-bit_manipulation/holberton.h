df2c2df9d41273ef789d225082dd62559cfcacf6
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-30 12:53:47 -0700

    Header file.

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -3,9 +3,13 @@
 
 /* Libraries */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /* Prototypes */
+int _putchar(char c);
+
 unsigned int binary_to_uint(const char *b);
 
 void print_binary(unsigned long int n);
