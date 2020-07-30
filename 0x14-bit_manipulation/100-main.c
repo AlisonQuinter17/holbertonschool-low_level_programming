f7e55a7013f67a469e687dbe4d5299e9a110d630
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-07-30 07:56:29 -0700

    Test files.

---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+int main(void)
+{
+    int n;
+
+    n = get_endianness();
+    if (n != 0)
+    {
+        printf("Little Endian\n");
+    }
+    else
+    {
+        printf("Big Endian\n");
+    }
+    return (0);
+}
