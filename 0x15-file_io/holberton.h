ce58873f18c6979af98501e3967e03ba2826ecf5
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-08-04 19:43:51 -0700

    Header file.

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,19 @@
+#ifndef FILE_H
+#define FILE_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Prototypes */
+int _putchar(char c);
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
