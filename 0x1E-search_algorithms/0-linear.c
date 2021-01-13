c46a2de0afa93ba4df87555d952c3b3deb0adcba
Alison Quintero <alisondanellaquintero@hotmail.com>
2021-01-13 12:16:31 -0500

    Linear search algorithm.

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	}
 
-	while (index <= size)
+	while (index < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
 		if (array[index] == value)
